b7d4be33ce58cd95d5f7c8616fd0f590a0db396e
jeo <abdeldjalildh990@gmail.com>
2023-03-18 19:23:39 +0100

    dis home kin

--- src/Home/KinHome.js
+++ src/Home/KinHome.js
@@ -6,7 +6,7 @@ import TopcloudErr from "../comp/topcloud_err";
 import Page404 from "../Page_404";
 import { auth } from "../firebase/config";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect } from "react";
+import { useEffect,useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Profile from "../comp/Profile";
 import { signOut, sendEmailVerification } from "firebase/auth";
@@ -56,8 +56,6 @@ const KinHome = () => {
   }
 
 
-
-
   const Prev = (e) => {
     
     
@@ -87,17 +85,42 @@ const KinHome = () => {
     }
     if (current_fs.id === "5") {
       document.querySelector(".progress_holder:nth-child(4)").classList.remove("activated_step");
-    }
-   
-    
+    } 
   
   }
   
 
+  const validButton = (eo,name) => {
+    eo.preventDefault();
+      name <=3? eo.target.disabled=true :  Next();
+  }
+
+  const countBOX = (eo,activite) => {
+    eo.target.checked? setactivites(++activite):setactivites(--activite)
+    activite <= 0 ? document.querySelectorAll(".nextStep")[4].disabled = true : document.querySelectorAll(".nextStep")[4].disabled = false;
+
+  }
+  const validButton2 = (eo,name) => {
+    eo.preventDefault();
+    activite <=0? eo.target.disabled=true : Next();
+  }
 
 
+  const validButton3 = (eo,amount) => {
+    eo.preventDefault();
+  if (amount < 2 || amount > 5) {
+    eo.target.disabled = true
+  } else {
+    Next();   setTimeout(() => {   document.getElementById("kin-dialog").close();   }, 3000);
+  }
+  
+  }
 
 
+  let [name, setname] = useState(0);
+  let [address, setaddress] = useState(0);
+  let [activite, setactivites] = useState(0);
+  let [amount, setamount] = useState(0);
 
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
@@ -168,23 +191,29 @@ const KinHome = () => {
                     </fieldset>
 
                     <fieldset className="step" id="1">
-                     <label>Kindergarten Name</label>
+                     <label>Kindergarten Name <p>*</p></label>
                      <br/>
                      <br/>
-                      <input  type="text" placeholder="Rawdhat:" required/>
-                      <button className="nextStep" onClick={(eo) => { eo.preventDefault();  Next();   }}>Next</button>
+                      <input  type="text" placeholder="Rawdhat:" required onChange={(eo) => {
+                        setname(eo.target.value.length)
+                        name <= 3 ? document.querySelectorAll(".nextStep")[1].disabled = true : document.querySelectorAll(".nextStep")[1].disabled = false;
+                      }}/>
+                      <button className="nextStep" onClick={(eo) => {     validButton(eo,name)   }}>Next</button>
                     </fieldset>
                     <fieldset className="step" id="2">
                       <button className="prevStep"  onClick={(eo) => {  eo.preventDefault(); Prev();   }}>Prev</button>
-                      <label>Kindergarten Address</label>
+                      <label>Kindergarten Address <p>*</p></label>
                       <br/>
                      <br/>
-                      <input  type="text" required placeholder="address" />
-                      <button className="nextStep"  onClick={(eo) => { eo.preventDefault();  Next();   }}>Next</button>
+                      <input  type="text" required placeholder="Address"  onChange={(eo) => {
+                        setaddress(eo.target.value.length)
+                        address <= 3 ? document.querySelectorAll(".nextStep")[2].disabled = true : document.querySelectorAll(".nextStep")[2].disabled = false;
+                      }}/>
+                      <button className="nextStep"  onClick={(eo) => { validButton(eo,address);   }}>Next</button>
                     </fieldset>
                     <fieldset className="step" id="3">
                       <button className="prevStep"  onClick={(eo) => { eo.preventDefault();  Prev();   }}>Prev</button>
-                      <label>Kindergarten Picture</label>
+                      <label>Kindergarten Picture <p style={{fontSize:"15px"}}>(Not Obligatory)</p></label>
                       <br/>
                      <br/>
                       <input  type="file" placeholder="add picture"/>
@@ -193,7 +222,7 @@ const KinHome = () => {
                     <fieldset className="step" id="4">
                       <button className="prevStep" onClick={(eo) => { eo.preventDefault();  Prev();   }}>Prev</button>
                       <div className="kin-form-header" >
-                      <label>Kindergarten Activites</label>
+                      <label>Kindergarten Activites <p>*</p></label>
                       <br/> <br/> 
                       <p>Choose the activites that your kindergarten do :</p>
                       </div>
@@ -202,26 +231,59 @@ const KinHome = () => {
 
                     <div className="activites">
 
-                    <div className="activite"> <input type="checkbox" id="Travel"/> <lable for="Travel">Travel</lable> </div>  
-                     <div className="activite"> <input type="checkbox" id="Language Learning"/> <lable for="Language Learning">Language Learning</lable> </div>
-                     <div className="activite"> <input type="checkbox" id="Sports"/> <lable for="Sports">Sports</lable> </div>
-                     <div className="activite"> <input type="checkbox" id="Painting "/> <lable for="Painting ">Painting </lable> </div>
-                     <div className="activite"> <input type="checkbox" id="Quran "/> <lable for="Quran ">Quran </lable> </div>
-                     <div className="activite"> <input type="checkbox" id="Reading "/> <lable for="Reading ">Reading </lable> </div>
-                     <div className="activite"> <input type="checkbox" id="other "/> <lable for="other ">Other Things </lable> </div>
+                    <div className="activite"> <input type="checkbox" id="Travel" onChange={(eo) => {
+                     countBOX(eo,activite);
+                    }}/> <lable for="Travel">Travel</lable> </div>  
+
+
+                     <div className="activite"> <input type="checkbox" id="Language Learning"onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="Language Learning">Language Learning</lable> </div>
+
+
+                     <div className="activite"> <input type="checkbox" id="Sports"onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="Sports">Sports</lable> </div>
+
+
+                     <div className="activite"> <input type="checkbox" id="Painting "onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="Painting ">Painting </lable> </div>
+
+
+                     <div className="activite"> <input type="checkbox" id="Quran "onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="Quran ">Quran </lable> </div>
+
+
+                     <div className="activite"> <input type="checkbox" id="Reading "onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="Reading ">Reading </lable> </div>
+
+
+                     <div className="activite"> <input type="checkbox" id="other "onChange={(eo) => {
+                      countBOX(eo,activite);
+                     }}/> <lable for="other ">Other Things </lable> </div>
 
                     </div>
-                      <button className="nextStep"  onClick={(eo) => {  eo.preventDefault(); Next();   }}>Next</button>
+                      <button className="nextStep" onClick={(eo) => { validButton2(eo,activite)  }}>Next</button>
                     </fieldset>
+
                     <fieldset className="step" id="5">
                       <button className="prevStep"  onClick={(eo) => { eo.preventDefault();  Prev();   }}>Prev</button>
-                      <label>kindergarten monthly payment amount</label>
+                      <label>kindergarten monthly payment amount <p>*</p></label>
                       <br/> <br/>
-                      <input  type="number" placeholder="DA/MONTH"/>
-                      <button className="nextStep"  onClick={(eo) => {  eo.preventDefault();  Next();
-                      setTimeout(() => {
-                        document.getElementById("kin-dialog").close();
-                      }, 3000);
+                      <div className="amount">
+                      <input  type="number" placeholder="Price" min="500" max="10000" step="50" style={{width:"200px"}}
+                      onChange={ (eo) => {
+                        setamount(eo.target.value.length)
+                        amount < 2 ? document.querySelectorAll(".nextStep")[5].disabled = true : document.querySelectorAll(".nextStep")[5].disabled = false;
+                      }} />
+                       <span class="unit">.00 DA</span>
+                      </div>
+                      
+                      <button className="nextStep"  onClick={(eo) => {  
+                        validButton3(eo,amount);
                       }}>Next</button>
                     </fieldset>
 

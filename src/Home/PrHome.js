46ab904771324d3cd641754b209528d4a93f274b
jeo <abdeldjalildh990@gmail.com>
2023-03-21 15:33:09 +0100

    design parente home

--- src/Home/PrHome.js
+++ src/Home/PrHome.js
@@ -11,44 +11,32 @@ import { useNavigate } from "react-router-dom";
 import { signOut, sendEmailVerification } from "firebase/auth";
 
 const PrHome = () => {
-
-
   const h1 = document.getElementById("h1"),
-  banner = document.getElementById("banner"),
-  button = document.getElementById("button");
+    banner = document.getElementById("banner"),
+    button = document.getElementById("button");
 
   const onScroll = (event) => {
-  
-  const scrollPosition = event.target.scrollingElement.scrollTop;
-  if (scrollPosition > 150) {
-    banner.style.backgroundSize = "130%";
-    h1.style.opacity = 0;
-    h1.style.translate = "0 -50px";
-    h1.style.scale = "0.9";
-    button.style.opacity = 0;
-    button.style.translate = "0 -50px";
-    button.style.scale = "0.8";
-  } else {
-    banner.style.backgroundSize = "100%";
-    h1.style.opacity = 1;
-    h1.style.translate = 0;
-    h1.style.scale = 1;
-    button.style.opacity = 1;
-    button.style.translate = 0;
-    button.style.scale = 1;
-  }
-};
+    const scrollPosition = event.target.scrollingElement.scrollTop;
+    if (scrollPosition > 150) {
+      banner.style.backgroundSize = "130%";
+      h1.style.opacity = 0;
+      h1.style.translate = "0 -50px";
+      h1.style.scale = "0.9";
+      button.style.opacity = 0;
+      button.style.translate = "0 -50px";
+      button.style.scale = "0.8";
+    } else {
+      banner.style.backgroundSize = "100%";
+      h1.style.opacity = 1;
+      h1.style.translate = 0;
+      h1.style.scale = 1;
+      button.style.opacity = 1;
+      button.style.translate = 0;
+      button.style.scale = 1;
+    }
+  };
 
-document.addEventListener("scroll", onScroll);
-
-
-
-
-
-
-
-
-
+  document.addEventListener("scroll", onScroll);
 
   const [user, loading, error] = useAuthState(auth);
   const navigate = useNavigate();
@@ -68,7 +56,6 @@ document.addEventListener("scroll", onScroll);
     );
   }
 
-
   if (error) {
     //if he is in the loading state do this block of code ... and when he done read the rest of the code.
     return (
@@ -77,8 +64,6 @@ document.addEventListener("scroll", onScroll);
       </>
     );
   }
-
-
 
   if (!user) {
     navigate("/Visitor");
@@ -127,19 +112,57 @@ document.addEventListener("scroll", onScroll);
 
           <Profile />
           <div className="main appmain">
-           
             <div id="banner" className="banner">
-               <h1 id="h1">welcome {user.displayName}</h1>
-               <button id="button" onClick={(eo) => {
-                onScroll(eo)
-               }}>Get Started</button>
+              <h1 id="h1">welcome {user.displayName}</h1>
+
+              <div className="search-container">
+                <input className="search-main" />
+
+                  <span className="searchicon" />
+                  
+                  <div className="microphone" onClick={(eo) => {
+                    //if&else
+
+                   const tester = document.querySelector(".icon");
+                   const icons = document.querySelectorAll(".icon");
+                    
+                   icons.forEach(icon => {
+                    icon.style.animation = "ani 2.2s ease-out infinite";
+                   });
+
+                    
+                  }}>
+                     <label style={{cursor:"pointer"}}> search by</label>
+                  <span class="material-symbols-outlined">  filter_list  </span>
+               </div>
+                
+                <div className="icon-holder">
+                  <div className="icon" id="Price">
+                  <span class="material-symbols-outlined circ">attach_money</span>
+                  <div className="tooltip">Price</div>
+                  </div>
+                  <div className="icon" id="Place">
+                  <span class="material-symbols-outlined circ">location_on</span>
+                    <div className="tooltip">Place</div>
+                  </div>
+                  <div className="icon" id="Activ">
+                  <span class="material-symbols-outlined circ">extension</span>
+                    <div className="tooltip">Activite</div>
+                  </div>
+                  <div className="icon" id="Trending">
+                    <span className="material-symbols-outlined circ">star</span>
+                    <div className="tooltip">Trending</div>
+                  </div>
+                  <div className="icon" id="All">
+                    <div className="dots" />
+                    <div className="tooltip">All</div>
+                  </div>
+                </div>
+              </div>
             </div>
-          
+
           </div>
           <Botcloud />
-          
-          
-
         </>
       );
     } else {
